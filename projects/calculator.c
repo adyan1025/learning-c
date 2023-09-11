9db5aec16fd1de22128c9b93ddff3d73ffd3d4ae
Adyan Chowdhury <127169193+adyan1025@users.noreply.github.com>
2023-09-10 23:20:31 -0400

    Add files via upload

---
+++ projects/calculator.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+
+int main() {
+    char op;
+    int x;
+    int y;
+    int result;
+    
+    printf("Choose the operator:\n");
+    scanf("%c", &op);
+    
+    if (op == '+') {
+        printf("Choose the first number:\n");
+        scanf("%d", &x);
+    
+        printf("Choose the second number: \n");
+        scanf("%d", &y);
+
+        result = x + y;
+        printf("%d", result);
+    }
+    else if (op == '-') {
+        printf("Choose the first number:\n");
+        scanf("%d", &x);
+        
+        printf("Choose the second number: \n");
+        scanf("%d", &y);
+
+        result = x - y;
+        printf("%d", result);
+    }
+    else if (op == '*') {
+        printf("Choose the first number:\n");
+        scanf("%d", &x);
+        
+        printf("Choose the second number: \n");
+        scanf("%d", &y);
+
+        result = x * y;
+        printf("%d", result);
+    }
+    else if (op == '/') {
+        printf("Choose the first number:\n");
+        scanf("%d", &x);
+        
+        printf("Choose the second number: \n");
+        scanf("%d", &y);
+
+        result = x/y;
+        printf("%d", result);
+    }
+    else {
+        printf("Please choose a valid operation.");
+    }
+    
+    return 0;
+}
